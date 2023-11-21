8c4f0ed71dd2b7b5f729379bb3ec2d49b6ed0d4a
dman82499 <devan.mallory@yahoo.com>
2023-11-21 16:46:43 -0500

    Updated Documentation to be consistent with new versions

--- BluetoothReciever/data_collection/device_setup.py
+++ BluetoothReciever/data_collection/device_setup.py
@@ -5,8 +5,13 @@ from data_collection import bluetooth_reciver
     by inputting a Name, function, and UUID
 
     2. Create an MSenseDevice object, and add all desired characteristics to it through a list.
+    
+    3. Add this MSenseDevice object to the 'all_sensors' list
 
-A Default Example using the Ohio State SENSE lab devices has been done for you below'''
+A Default Example using the Ohio State SENSE lab MotionSenseHRV devices has been done for you below. 
+Note that the encoding in the packets (located in bluetooth_reciver.py) is specifically done because we have created a 
+specific packet format that encodes data in a way that packs everything together, to minimize packet sizes.
+This format can be found in Configuration_Firmware.docx, and sections are also commented in blueooth reciver.py'''
 
 ppg_sensor = bluetooth_reciver.MSenseCharacteristic("PPG", bluetooth_reciver.ppg_handler,
                                                     "da39c925-1d81-48e2-9c68-d0ae4bbd351f")
@@ -37,7 +42,7 @@ LBioZSensor = bluetooth_reciver.MSenseDevice("LBIOZ", [MagnitudeImpedance])
 RBioZSensor = bluetooth_reciver.MSenseDevice("RBIOZ", [MagnitudeImpedance])
 
 
-
+# if you would like to add a new MSense device, please add it here
 all_sensors = [ RightSensor, LeftSensor, LBioZSensor, RBioZSensor ]
 
 
